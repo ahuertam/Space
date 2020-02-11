2d96180c9428dcc70c54d261b4329910f4265ab5
Alejandro Huerta <ahuertam@yahoo.es>
2020-02-11 15:05:33 +0100

    feat: rotating head

---
+++ space/Assets/Scripts/LookDirection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class LookDirection : MonoBehaviour
+{
+  private Vector3 lastPos;
+  private Vector3 velocity;
+  private void Start()
+  {
+    lastPos = transform.position;
+    velocity = transform.position - lastPos;
+  }
+  private void FixedUpdate()
+  {
+    velocity = transform.position - lastPos;
+    lastPos = transform.position;
+    lookTowardDirectionOfVelocity();
+  }
+  void lookTowardDirectionOfVelocity()
+  {
+    transform.rotation = Quaternion.LookRotation(velocity);
+  }
+}
